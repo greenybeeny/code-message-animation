abd09d457f823d4399901e024c65b692ccccb47a
greenybeeny <92817241+greenybeeny@users.noreply.github.com>
2021-10-29 14:03:44 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,108 +1,52 @@
-basic.forever(function () {
-    basic.showLeds(`
-        . # . . .
-        . . . . .
-        . . . . .
-        # . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . # . . .
-        . . . . .
-        . . . . .
-        # # . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . # . . .
-        . . . . .
-        . . . . .
-        # # # . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . # . . .
-        . . . . .
-        . . . . .
-        # # # # .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . # . . .
-        . . . . .
-        . . . . .
-        . # # # #
-        . . . . .
-        `)
-    basic.showLeds(`
-        . # . . .
-        . . . . .
-        . . . . #
-        . . # # #
-        . . . . .
-        `)
-    basic.showLeds(`
-        . # . . .
-        . . . . #
-        . . . . #
-        . . . # #
-        . . . . .
-        `)
-    basic.showLeds(`
-        . # . . .
-        . . . # #
-        . . . . #
-        . . . . #
-        . . . . .
-        `)
-    basic.showLeds(`
-        . # . . .
-        . . # # #
-        . . . . #
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . # . . .
-        . # # # #
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . # . . .
-        . # # # .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . # . . .
-        . # # . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . # . . .
-        . # . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . # . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showString("YUM!")
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 3; index++) {
+        basic.showLeds(`
+            . # # # .
+            # # . # #
+            # . # . #
+            # # . # #
+            . # # # .
+            `)
+        basic.pause(200)
+        basic.showLeds(`
+            . # # # .
+            # . # . #
+            # # . # #
+            # . # . #
+            . # # # .
+            `)
+        basic.pause(200)
+    }
+    basic.pause(200)
+    basic.showString("SET AN ALARM!")
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 3; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # . . .
+            . # . . .
+            . # # . .
+            `)
+        basic.pause(200)
+        basic.showLeds(`
+            . . . . .
+            # # # # .
+            . . # . .
+            . # . . .
+            # # # # .
+            `)
+        basic.pause(200)
+        basic.showLeds(`
+            # # # # #
+            . . . # .
+            . . # . .
+            . # . . .
+            # # # # #
+            `)
+        basic.pause(200)
+    }
+    basic.pause(200)
+    basic.showString("Sleep Well!")
 })
